7f1c33dd994039d7ad41bbfda55e877a5244b337
Scott Mathson <25587929+scottmathson@users.noreply.github.com>
2019-06-24 16:51:35 -0600

    adding schema markup

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,29 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="https://cdn.auth0.com/styleguide/components/2.0.0/components.min.css" />
           <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+          <script type="application/ld+json">
+            {
+                "@context": "http://schema.org",
+                "@type": "WebPage",
+                "name": "Identicons - Royalty-free, free Identity icons from Auth0",
+                "description": "Get royalty-free identity icons. A visual language for identity - open source project from Auth0’s Guillermo Mesyngier and Vittorio Bertocci. For use in presentations, specifications and documentation.",
+                "url": "https://identicons.dev/",
+                "image": "https://identicons.dev/static/favicons/apple-touch-icon.png",
+                "publisher": {
+                    "@type": "Organization",
+                    "name": "Auth0",
+                    "legalName": "Auth0 Inc.",
+                    "url": "https://auth0.com/",
+                    "logo": "https://cdn.auth0.com/website/assets/pages/press/img/resources/auth0-logo-main-6001cece68.svg",
+                    "foundingDate": "2013",
+                    "sameAs": [
+                        "https://twitter.com/auth0",
+                        "https://www.facebook.com/getauth0/",
+                        "https://www.linkedin.com/company/auth0"
+                    ]
+                }
+              }
+          </script>
           <meta name="google-site-verification" content="IXxB6hv45d2ZH_S9bHfRP5M45c9_K9287ia6saqJXvw" />
         </Head>
         <body className="identicons-page">
